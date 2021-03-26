89dfe9d3ce80cda9388f9411e272ac547b30ba97
manojthetechguy <manojthetechguy@gmail.com>
2021-03-26 17:49:34 +0530

    eslint

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,6 +1,5 @@
 import Heading from "./components/heading/heading";
 import HelloWorld from "./components/hello-world-button/hello-world-button";
-import React from "react";
 
 const heading = new Heading();
 const helloWorld = new HelloWorld();
