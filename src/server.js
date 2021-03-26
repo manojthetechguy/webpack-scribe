b1d95b82e4d8e2e66d6ac8ace0dd20d6d614b268
manojthetechguy <manojthetechguy@gmail.com>
2021-03-26 15:14:56 +0530

    express -mpa

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/hello-world/", function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/hello-world.html");
+  const contents = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contents);
+});
+
+app.get("/kiwi/", function (req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/kiwi.html");
+  const contents = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contents);
+});
+
+app.use(
+  "/static",
+  express.static(path.resolve(path.resolve(__dirname, "../dist")))
+);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.info(`Server listening at PORT: ${PORT}`);
+});
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "",
+    publicPath: "/static/",
   },
   mode: "development",
   devServer: {
